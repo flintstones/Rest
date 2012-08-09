74ae4faf402b2a5c96ecda51d5e67aa6543e9eb7
Igor Wiedler <igor@wiedler.ch>
2012-08-09 22:21:02 +0200

    Ammendments to composer.json patch

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,24 +1,5 @@
 <?php
 
-/*
- * This file is part of the Flintstones RestServiceProvider.
- *
- * (c) Igor Wiedler <igor@wiedler.ch>
- *
- * For the full copyright and license information, please view the LICENSE
-  * file that was distributed with this source code.
- */
-
-$file = __DIR__.'/../vendor/autoload.php';
-if (!file_exists($file)) {
-  throw new RuntimeException('Install dependencies to run test suite.');
-}
-
-$autoload = require_once $file;
-
-use Symfony\Component\ClassLoader\UniversalClassLoader;
-
-$loader = new UniversalClassLoader();
-$loader->registerNamespace('Flintstones\Rest', __DIR__.'/../src');
-$loader->registerNamespace('Flintstones\Tests\Rest', __DIR__);
-$loader->register();
+$loader = require __DIR__.'/../vendor/autoload.php';
+$loader->add('FlintstonesRest', __DIR__.'/../src');
+$loader->add('Flintstones\Tests\Rest', __DIR__);
