e343df083a8ad8d6bc09dc635e476a44ea781636
Toni Van de Voorde <toni.vdv@gmail.com>
2012-08-09 21:00:11 +0200

    Migrate to composer

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -9,7 +9,12 @@
   * file that was distributed with this source code.
  */
 
-require_once __DIR__.'/../vendor/silex/autoload.php';
+$file = __DIR__.'/../vendor/autoload.php';
+if (!file_exists($file)) {
+  throw new RuntimeException('Install dependencies to run test suite.');
+}
+
+$autoload = require_once $file;
 
 use Symfony\Component\ClassLoader\UniversalClassLoader;
 
